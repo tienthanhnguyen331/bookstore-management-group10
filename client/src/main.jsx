e0613db6a347b1ac43b5592260ae5ce8fe78769d
TienThanhNguyen <nguyentienthanhaeaefff@gmail.com>
2025-11-27 23:30:55 +0700

    Re-init: Recreated project structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,1 +1,10 @@
-// Điểm khởi chạy (Bọc AuthProvider)
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
