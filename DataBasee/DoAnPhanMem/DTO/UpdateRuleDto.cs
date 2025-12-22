932ff7e09c4235d73f8fff25293b2962b27746f2
TienThanhNguyen <nguyentienthanhaeaefff@gmail.com>
2025-12-22 20:59:05 +0700

    Remove DataBasee folder from Frontend branch tracking

--- DataBasee/DoAnPhanMem/DTO/UpdateRuleDto.cs
+++
@@ -1,8 +0,0 @@
-namespace DoAnPhanMem.DTO
-{
-    public class UpdateRuleDto
-    {
-        public string GiaTri { get; set; } = string.Empty;
-        public bool TrangThai { get; set; }
-    }
-}
