07547ff6615f08cb402cfa5f254aabde49e50da9
TienThanhNguyen <nguyentienthanhaeaefff@gmail.com>
2026-01-17 15:12:43 +0700

    Allow Vercel domain in CORS policy

--- DataBasee/DoAnPhanMem/Program.cs
+++ DataBasee/DoAnPhanMem/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173")
+            policy.WithOrigins("http://localhost:5173", "https://bookstore-management-group10-25ds.vercel.app") // Add Vercel domain
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
